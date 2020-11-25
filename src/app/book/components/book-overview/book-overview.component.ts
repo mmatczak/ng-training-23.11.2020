16412d970c135291b937005be76be56ef65b56a7
Marek Matczak <mmatczak@autograf.pl>
2020-11-25 09:42:51 +0100

    Typeahead implemented

--- src/app/book/components/book-overview/book-overview.component.ts
+++ src/app/book/components/book-overview/book-overview.component.ts
@@ -1,23 +1,27 @@
-import {Component, OnDestroy} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {Book} from '../../model/book';
 import {BookService} from '../../services/book.service';
-import {Subscription} from 'rxjs';
+import {fromEvent, Observable, of, OperatorFunction} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'ba-book-overview',
   templateUrl: './book-overview.component.html',
   styleUrls: ['./book-overview.component.scss']
 })
-export class BookOverviewComponent implements OnDestroy {
+export class BookOverviewComponent implements AfterViewInit{
+  @ViewChild('searchInput')
+  searchInput: ElementRef<HTMLInputElement>;
+
   books: Book[];
+  readonly books$: Observable<Book[]>;
+
+  results$: Observable<string[]>;
 
   selectedBook: Book | null = null;
 
-  private readonly subscription: Subscription;
-
   constructor(books: BookService) {
-    this.subscription = books.getAll()
-      .subscribe(allBooks => this.books = allBooks);
+    this.books$ = books.getAll();
   }
 
   selectBook(book: Book): void {
@@ -34,7 +38,24 @@ export class BookOverviewComponent implements OnDestroy {
     this.selectedBook = changedBook;
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  ngAfterViewInit(): void {
+    function search(query: string): Observable<string[]> {
+      return of([query]);
+    }
+
+    this.results$ = fromEvent(this.searchInput.nativeElement, 'input')
+      .pipe(
+        mapFromInputEventToInputValue(),
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap(query => search(query))
+      );
   }
+}
+
+function mapFromInputEventToInputValue(): OperatorFunction<Event, string> {
+  return map(event => {
+    const inputElement = event.target as HTMLInputElement;
+    return inputElement.value;
+  });
 }
